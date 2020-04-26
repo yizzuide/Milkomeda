7138197d44e164289ed1962e970f8ec7e7c43256
doujiang <>
2020-04-26 18:25:17 +0800

    添加Redis 分布式锁

---
+++ Milkomeda/src/main/java/com/github/yizzuide/milkomeda/wormhole/EnableWormhole.java
@@ -0,0 +1,11 @@
+package com.github.yizzuide.milkomeda.wormhole;
+
+
+/**
+ * EnableWormhole
+ * @author jsq
+ * @since 1.16.0
+ * Create at 2019/11/23 00:18
+ */
+public interface EnableWormhole {
+}
