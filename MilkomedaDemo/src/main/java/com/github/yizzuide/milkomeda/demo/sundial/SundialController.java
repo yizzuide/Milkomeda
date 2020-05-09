0bed03d72455884408281b18cbc3ce90fa194645
jiasqi <>
2020-05-09 15:55:31 +0800

    动态数据源测试用例 事物测试

--- MilkomedaDemo/src/main/java/com/github/yizzuide/milkomeda/demo/sundial/SundialController.java
+++ MilkomedaDemo/src/main/java/com/github/yizzuide/milkomeda/demo/sundial/SundialController.java
@@ -1,13 +1,18 @@
 package com.github.yizzuide.milkomeda.demo.sundial;
 
+import com.github.yizzuide.milkomeda.demo.halo.domain.TOrder;
+import com.github.yizzuide.milkomeda.demo.halo.mapper.TOrderMapper;
+import com.github.yizzuide.milkomeda.demo.sundial.service.DataSourceService;
 import com.github.yizzuide.milkomeda.fusion.Fusion;
 import com.github.yizzuide.milkomeda.sundial.SundialDynamicDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
+import java.util.Date;
 
 /**
  * @date: 2020/5/8
@@ -23,18 +28,38 @@ public class SundialController {
     @Autowired
     private DataSource dataSource;
 
-    @SundialDynamicDataSource
+    @Autowired
+    private DataSourceService dataSourceService;
+    @Autowired
+    private TOrderMapper tOrderMapper;
+
+    
     @Fusion
     @RequestMapping("get")
-    private String getData() {
+    public Object getData() {
         log.info("get datasource: {}", dataSource.getClass());
-        return "OK";
+//        TOrder tOrder = new TOrder();
+//        tOrder.setOrderNo(123L);
+//        tOrder.setCreateTime(new Date());
+//        tOrder.setProductId(1L);
+//        tOrder.setProductName("测试");
+//        tOrder.setUserId(222L);
+//        dataSourceService.insert(tOrder);
+        return  dataSourceService.queryAll(new TOrder());
     }
 
-    @SundialDynamicDataSource(value = "read-only")
+
     @RequestMapping("slave")
-    private String slave() {
+    public Object slave() {
         log.info("get datasource: {}", dataSource.getClass());
-        return "OK";
+//        TOrder tOrder = new TOrder();
+//        tOrder.setOrderNo(123L);
+//        tOrder.setCreateTime(new Date());
+//        tOrder.setProductId(1L);
+//        tOrder.setProductName("测试");
+//        tOrder.setUserId(222L);
+//        dataSourceService.insert(tOrder);
+
+        return  tOrderMapper.queryAll(new TOrder());
     }
 }
