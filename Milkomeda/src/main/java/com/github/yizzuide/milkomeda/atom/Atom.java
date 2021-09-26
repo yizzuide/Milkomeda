80ebf915792253a2d592e428c3e1f140634d37de
yizzuide <fu837014586@163.com>
2021-09-26 18:47:10 +0800

    chore(doc): Add license for all source file.

--- Milkomeda/src/main/java/com/github/yizzuide/milkomeda/atom/Atom.java
+++ Milkomeda/src/main/java/com/github/yizzuide/milkomeda/atom/Atom.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2021 yizzuide All rights Reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.yizzuide.milkomeda.atom;
 
 import java.time.Duration;
@@ -34,7 +55,7 @@ public interface Atom {
     AtomLockInfo tryLock(String keyPath, AtomLockType type, boolean readOnly) throws Exception;
 
     /**
-     * 尝试去获取锁
+     * 在等待时间内尝试去获取锁
      * @param keyPath   键路径
      * @param waitTime  尝试时间
      * @param leaseTime 获取锁后的解锁时间
